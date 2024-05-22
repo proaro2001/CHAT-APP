5b1441200b5c637eee763925e8d3a68befb3f788
proaro2001 <proaro2001@gmail.com>
2024-05-22 14:32:14 -0700

    Add signup feature

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,3 +1,14 @@
+/**
+ * Represents the user schema for the chat application.
+ *
+ * @typedef {Object} UserSchema
+ * @property {string} fullName - The full name of the user.
+ * @property {string} username - The username of the user.
+ * @property {string} password - The password of the user.
+ * @property {string} gender - The gender of the user. Can be either "male" or "female".
+ * @property {string} profilePic - The profile picture of the user. Defaults to an empty string.
+ */
+
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
