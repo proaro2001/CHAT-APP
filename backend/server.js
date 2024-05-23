9a958e1806563227d581eb81cfc4bd52e20172b4
proaro2001 <proaro2001@gmail.com>
2024-05-23 16:38:49 -0700

    Add Get Users for Sidebar Endpoint

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,11 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
+// routes
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 // variables
@@ -18,6 +21,7 @@ app.use(cookieParser()); // parse cookies
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 // listener for the server
 app.listen(PORT, () => {
