f79b6038c42427f164a0fd2610a7e9dab6d17f2b
proaro2001 <proaro2001@gmail.com>
2024-06-03 16:55:46 -0700

    Add build command to npm package.json file

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 // routes
 import authRoutes from "./routes/auth.routes.js";
@@ -13,6 +14,9 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5100; // PORT is either the one provided by the environment or 5000
 
+// for deployment
+const __dirname = path.resolve(); //
+
 // middleware
 dotenv.config(); // load env variables
 
@@ -22,6 +26,12 @@ app.use(cookieParser()); // parse cookies
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 // listener for the server
 server.listen(PORT, () => {
