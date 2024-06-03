caab36ab529088d010cc25001f0748c70ef54c7b
proaro2001 <proaro2001@gmail.com>
2024-06-03 16:38:56 -0700

    shake feature

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -11,6 +11,8 @@ const Message = ({ message }) => {
     : selectedConversation.profilePic;
   const bubbleBgColor = fromMe ? "bg-blue-500" : "";
   const formattedTime = extractTime(message.createdAt);
+  const shakeClass = message.shouldShake ? "shake" : "";
+
   return (
     <div className={`chat ${chatClassName}`}>
       <div className="chat-image avatar">
@@ -18,7 +20,7 @@ const Message = ({ message }) => {
           <img alt="user avatar" src={profilePic} />
         </div>
       </div>
-      <div className={`chat-bubble text-white ${bubbleBgColor}`}>
+      <div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass}`}>
         {message.message}
       </div>
       <div className="chat-footer opacity-50 text-xs flex gap-1 items-center text-slate-900">
